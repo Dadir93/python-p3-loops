c3293d8b7296925d5fe6a96309d8d2ed47a38fc6
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-03-08 15:29:05 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def happy_new_year():
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+def reverse_string(string):
+    return string[::-1]
+
+def square_integers(int_list):
+    return [num ** 2 for num in int_list]
+
+# Test the functions
+if __name__ == "__main__":
+    happy_new_year()
+    fizzbuzz_printer()
+    print(reverse_string("Hello, World!"))
+    print(square_integers([1, 2, 3, 4, 5]))
